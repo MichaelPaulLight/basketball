a2a1a751ca5c6fe212c8131f29f9c31f1b33fd25
qadabie18 <adabieq@umich.edu>
2025-03-02 22:01:56 -0500

    Adding in files used for unsupervised learning

--- Milestone2/Clustering Dendogram.py
+++ Milestone2/Clustering Dendogram.py
@@ -1,9 +1,15 @@
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import StandardScaler
 from scipy.cluster.hierarchy import dendrogram
 from sklearn.cluster import AgglomerativeClustering
+from yellowbrick.cluster import KElbowVisualizer 
+from scipy.stats import zscore
 
-
+pd.set_option('display.max_columns', None)
 
 def plot_dendrogram(model, **kwargs):
     n_samples = len(model.labels_)
@@ -11,8 +17,8 @@ def plot_dendrogram(model, **kwargs):
     for i, merge in enumerate(model.children_):
         current_count = 0
         for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1 
+            if (child_idx < n_samples):
+                current_count += 1
             else:
                 current_count += counts[child_idx - n_samples]
         counts[i] = current_count
@@ -20,53 +26,89 @@ def plot_dendrogram(model, **kwargs):
     linkage_matrix = np.column_stack(
         [model.children_, model.distances_, counts]
     ).astype(float)
+    
+    # Plot the dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+    
+    
+    # Add cluster labels
+    if 'labels' in kwargs:
+        for i, d in zip(kwargs['labels'], linkage_matrix):
+            plt.text(d[0], d[1], str(i), ha='center', va='bottom')
 
 
 
 if __name__ == '__main__':
 #import argparse 
-    import pandas as pd
-    import seaborn as sns
-    from sklearn.metrics import silhouette_score
-    from sklearn.metrics.pairwise import cosine_similarity
-    from sklearn.preprocessing import StandardScaler
+
     #parser = argparse.ArgumentParser()
     #parser.add_argument('input', help='input file')
     #args = parser.parse_args()
+    df0 = pd.read_parquet('data/21_22merged_player_stats.parquet')
     df1 = pd.read_parquet('data/22_23merged_player_stats.parquet')
     df2 = pd.read_parquet('data/23_24merged_player_stats.parquet')
     df3 = pd.read_parquet('data/24_25merged_player_stats.parquet')
-    df = pd.concat([df1, df2, df3])
-    X = df[['AVG_2_DEF_DIST', 'AVG_3_DEF_DIST', 'FG2_PCT', 'FG3_PCT', 'FG_Split', 'DREB','STL','BLK','PF','FG2Target','FG3Target','FGTaget','2FG%diff','3FG%diff','Poss/Game']]
+    df = pd.concat([df0, df1, df2, df3])
+    avg_fg3_pct = df['FG3_PCT'].mean()
+    df.loc[df['FG3Target'] < 0.025, 'FG3_PCT'] = avg_fg3_pct
+    df.loc[df['FG3_PCT']== 0, 'FG3_PCT'] = avg_fg3_pct
+    df['AVG_2_DIST'] = df['AVG_2_DEF_DIST'] - ((df['WINGSPAN']-(df['HEIGHT_WO_SHOES']/48))/24)
+    df['AVG_3_DIST'] = df['AVG_3_DEF_DIST'] - ((df['WINGSPAN']-(df['HEIGHT_WO_SHOES']/48))/24)
+    X = df[['FG2Target','FG2_PCT','AVG_2_DIST', 'FG3Target','FG3_PCT','AVG_3_DIST',  'DREB','BLK','PF','STL',]]
+    
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
     best_n_clusters = 0
     best_silhouette_score = -1
     best_fit_X = None
+    distortions = []
+    model = AgglomerativeClustering(linkage='ward',compute_distances=True)
+    visualizer = KElbowVisualizer(model, k=(2,20), timings=False)
+    visualizer.fit(X)
+    visualizer.show(outpath="elbow.png")
 
-    for n_clusters in range(8, 16):
-        model = AgglomerativeClustering(n_clusters=n_clusters,metric='cosine', linkage='single',compute_distances=True)
-        fit_X = model.fit_predict(X)
-        score = silhouette_score(X, fit_X)
-        if score > best_silhouette_score:
-            best_silhouette_score = score
-            best_n_clusters = n_clusters
-            best_fit_X = fit_X
+    model_best = AgglomerativeClustering(linkage='ward',n_clusters=9, compute_distances=True)
+    print(model_best.get_params())
+    model_best.fit(X)
+    
+    plt.figure(figsize=(10, 8))
+    plt.title("Hierarchical Clustering Dendrogram")
+    plot_dendrogram(model_best, truncate_mode="level", p=4, orientation='left')
+    plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+    plt.axvline(x=20, color='black', linestyle='--', label='threshold')
+    plt.legend(loc='upper left')
+    plt.show()
 
-    print(f"Best number of clusters: {best_n_clusters}")
-    print(f"Best silhouette score: {best_silhouette_score}")
-    fit_X = best_fit_X
-    X_df = pd.DataFrame(X, columns=['AVG_2_DEF_DIST', 'AVG_3_DEF_DIST', 'FG2_PCT', 'FG3_PCT', 'FG_Split', 'DREB','STL','BLK','PF','FG2Target','FG3Target','FGTaget','2FG%diff','3FG%diff','Poss/Game'])
+    fit_X = model_best.fit_predict(X)
+    X_df = pd.DataFrame(X, columns=['FG2Target','FG2_PCT','AVG_2_DIST', 'FG3Target','FG3_PCT','AVG_3_DIST',  'DREB','BLK','PF','STL',])
+    X_df[['AVG_2_DIST', 'AVG_3_DIST', 'FG2_PCT', 'FG3_PCT', 'PF','FG2Target','FG3Target']] = -1*X_df[['AVG_2_DIST', 'AVG_3_DIST', 'FG2_PCT', 'FG3_PCT', 'PF','FG2Target','FG3Target']]
     X_df['Cluster_Labels'] = fit_X
     df['Cluster_Labels'] = fit_X
+    cluster_dict = {0: '1. Perimeter Secondary Option',1: '3. The Marks',
+                    2: '5. Enforcers/Inside Help',3: '9. Pickpockets',
+                    4:'7. Perimeter and Inside Help', 5: '2. Perimeter Bad Defenders',
+                    6:'6. True Rim Protectors',7:'4. Risk Averse Paint Defender',
+                    8:'8. Anchors who Close Out'}
+    df['Cluster_Labels'] = df['Cluster_Labels'].map(cluster_dict)
+    X_df['Cluster_Labels'] = X_df['Cluster_Labels'].map(cluster_dict)
+    cluster_counts = df['Cluster_Labels'].value_counts()
+    print("Cluster counts:")
+    print(cluster_counts)
+    df.to_parquet('data/21-25player_clusters.parquet', index=False)
     cluster_averages = X_df.groupby('Cluster_Labels').mean()
     print(cluster_averages)
+    #plt.figure(figsize=(10, 8))
+    #sns.heatmap(cluster_averages, cmap='coolwarm', annot=True, fmt='.2f', y)
+    #plt.title('Cluster Averages Heatmap (Z-scores)')
+    #plt.xlabel('Features')
+    #plt.ylabel('Cluster Labels')
+    #plt.show()
     #plt.title("Hierarchical Clustering Dendrogram")
     # plot the top three levels of the dendrogram
     #plot_dendrogram(model, truncate_mode="level", p=3)
     #plt.xlabel("Number of points in node (or index of point if no parenthesis).")
-    sns.clustermap(cluster_averages, z_score=1)
-    plt.savefig('22-23data.png', dpi=300)
+    sns.clustermap(cluster_averages, z_score=1, cmap = 'vlag', row_cluster=False, col_cluster=False)
+    plt.savefig('21-25data.png', dpi=300)
     plt.show()
 
     player_name = input("Enter player's name (default: LeBron James): ") or "LeBron James"
@@ -80,8 +122,8 @@ if __name__ == '__main__':
         print(f"Player: {player_name}, Season: {season}, Cluster: {player_cluster}")
 
         # Calculate cosine similarity
-        player_vector = player_data[['AVG_2_DEF_DIST', 'AVG_3_DEF_DIST', 'FG2_PCT', 'FG3_PCT', 'FG_Split', 'DREB','STL','BLK','PF','FG2Target','FG3Target','FGTaget','2FG%diff','3FG%diff','Poss/Game']].values
-        all_vectors = df[['AVG_2_DEF_DIST', 'AVG_3_DEF_DIST', 'FG2_PCT', 'FG3_PCT', 'FG_Split', 'DREB','STL','BLK','PF','FG2Target','FG3Target','FGTaget','2FG%diff','3FG%diff','Poss/Game']].values
+        player_vector = player_data[['FG2Target','FG2_PCT','AVG_2_DIST', 'FG3Target','FG3_PCT','AVG_3_DIST','DREB','BLK','PF','STL',]].values
+        all_vectors = df[['FG2Target','FG2_PCT','AVG_2_DIST', 'FG3Target','FG3_PCT','AVG_3_DIST' 'DREB','BLK','PF','STL',]].values
         player_vector = scaler.transform(player_vector)
         all_vectors = scaler.transform(all_vectors)
         similarities = cosine_similarity(player_vector, all_vectors).flatten()
@@ -93,3 +135,10 @@ if __name__ == '__main__':
         print("Three most similar players:")
         for _, row in similar_players.iterrows():
             print(f"Player: {row['player_name']}, Season: {row['SEASON']}, Cluster: {row['Cluster_Labels']}")
+
+    # Find players with the lowest 'AVG_3_DEF_DIST'
+    # Print samples of every cluster with all columns
+    for cluster_label in df['Cluster_Labels'].unique():
+        print(f"\nCluster {cluster_label} samples:")
+        cluster_samples = df[df['Cluster_Labels'] == cluster_label].sample(n=3, random_state=42)
+        print(cluster_samples)
--- Milestone2/player_info_addition.py
+++ Milestone2/player_info_addition.py
@@ -1,4 +1,4 @@
-def player_info(seasons):
+def player_heights(players):
     import asyncio
     import aiohttp
     import pandas as pd
@@ -24,32 +24,28 @@ def player_info(seasons):
                             "Chrome/84.0.4147.89 Safari/537.36"}
 
 
-    async def fetch_play_by_play(session, season):
-        url = f"https://stats.nba.com/stats/draftcombineplayeranthro?LeagueID=00&SeasonYear={season}"
+    async def fetch_heights(session, player):
+        url = f"https://stats.nba.com/stats/commonplayerinfo?LeagueID=&PlayerID={player}"
         async with session.get(url, headers=headers) as response:
             if response.status == 200:
                 data = await response.json()
-                df =  pd.DataFrame(data['resultSets'][0]['rowSet'], columns=data['resultSets'][0]['headers'])
-                print(f"Processed game {season} with {len(df)} rows")
+                row =  pd.DataFrame(data['resultSets'][0]['rowSet'], columns=data['resultSets'][0]['headers'])
+                height = row['HEIGHT'].values[0]
+                value = {'PLAYER_ID': player, 'HEIGHT': height}
+                df = pd.DataFrame([value])
                 return df
             else:
-                print(f"Error fetching play-by-play data for game {season} from {url}: {response.status}")
                 return pd.DataFrame()
 
     async def main():
         async with aiohttp.ClientSession() as session:
             total_player_data = pd.DataFrame()
-            for season in seasons:
-                player_data = await fetch_play_by_play(session, season)
+            for player in players:
+                player_data = await fetch_heights(session, player)
                 total_player_data = pd.concat([total_player_data, player_data], ignore_index=True)
             # Concatenate all play-by-play data into a single DataFrame
             return total_player_data
             
-            
-            # Save to a parquet file
-            
-
-    # Run the main function
     result = asyncio.run(main())
     return result
 
@@ -61,10 +57,20 @@ if __name__ == '__main__':
     import os
     import pyarrow.parquet as pq
     import pyarrow as pa
-    seasons = list(range(2000, 2025))
-    player_data = player_info(seasons)
-    print(player_data.head(10))
-    pq.write_table(pa.Table.from_pandas(player_data), '00_24_anthro.parquet')
-    
+
+    file_path = 'data/00_24_anthro.parquet'
+    existing_data = pd.read_parquet(file_path)
+    missing_height_player_ids = existing_data[existing_data['HEIGHT_WO_SHOES'].isna()]['PLAYER_ID'].tolist()
+    print(f"Missing {len(missing_height_player_ids)} player heights")
+    heights = player_heights(missing_height_player_ids)
+    print(len(heights))
+    heights['HEIGHT_WO_SHOES'] = heights['HEIGHT'].apply(lambda x: (int(x.split('-')[0])*12 + int(x.split('-')[1]))-1.5 if x else None)
+    heights['WINGSPAN'] = heights['HEIGHT_WO_SHOES']*1.06
+    merged_data = existing_data.merge(heights, on='PLAYER_ID', how='left', suffixes=('','_temp'))
+    merged_data['HEIGHT_WO_SHOES'] = merged_data['HEIGHT_WO_SHOES'].fillna(merged_data['HEIGHT_WO_SHOES_temp'])
+    merged_data['WINGSPAN'] = merged_data['WINGSPAN'].fillna(merged_data['WINGSPAN_temp'])
+    merged_data = merged_data.drop(columns=['HEIGHT_WO_SHOES_temp', 'WINGSPAN_temp'])
+    print(len(merged_data['HEIGHT_WO_SHOES'].isna()))   
+    merged_data.to_parquet(file_path, index=False)
     
     
